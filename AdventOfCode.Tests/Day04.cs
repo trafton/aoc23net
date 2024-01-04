029d492cc2dbddf32d69cd1eb808b219bde6ee03
Trafton Haynes <traftonh@gmail.com>
2024-01-03 16:51:14 -0800

    remove inputs

--- AdventOfCode.Tests/Day04.cs
+++ AdventOfCode.Tests/Day04.cs
@@ -1,7 +1,102 @@
 namespace AdventOfCode.Tests;
 
 [TestFixture]
-public class Day04
+public class Day04Tests
 {
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Task1_TestData()
+    {
+        const string testStr = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\nCard 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\nCard 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\nCard 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+        var cards = testStr.Split("\n")
+            .Select(Card.Parse)
+            .Select(c => c.Points())
+            .Sum();
+        
+        Assert.That(cards, Is.EqualTo(13));
+    }
+
+    [Test]
+    public void Card_GenerateCopies()
+    {
+        const string testStr = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\nCard 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\nCard 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\nCard 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+        var cards = testStr.Split("\n")
+            .Select(Card.Parse);
+
+        var deck = Card.GenerateCopies(cards);
+
+        var totalCopies = deck.Sum();
+        
+        Assert.That(totalCopies, Is.EqualTo(30));
+    }
+
+// Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    [Test]
+    public void Card_ParseTestCard()
+    {
+        const string testStr = "Card   1:  4 33 89 61 95 36  5 30 26 55 | 15 33 28 36 93 57 26 13 95  4 18 79  6 87 60 66 69 67 19 42 22 61 78  5 58";
+
+        var result = Card.Parse(testStr);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Id, Is.EqualTo(1));
+            
+            Assert.That(result.Numbers, Has.Count.EqualTo(10));
+            Assert.That(result.Winners, Has.Count.EqualTo(25));
+        });
+    }
+
+    [Test]
+    public void Card_Matches()
+    {
+        const string testStr = "Card 1: 41 48 83  | 83 86";
+
+        var card = Card.Parse(testStr);
+        var result = card.Matches().ToList();
+        
+        Assert.That(result, Has.Count.EqualTo(1));
+        Assert.That(result, Contains.Item(83));
+    }
+    
+    [Test]
+    public void Card_Power_1()
+    {
+        const string testStr = "Card 1: 41 48 83  | 83 86";
+
+        var card = Card.Parse(testStr);
+        var result = card.Points();
+        
+
+        Assert.That(result, Is.EqualTo(1));
+    }
+    
+    [Test]
+    public void Card_Power_0()
+    {
+        const string testStr = "Card 1: 41 48 | 83 86";
+
+        var card = Card.Parse(testStr);
+        var result = card.Points();
+
+        Assert.That(result, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void Card_Power_5()
+    {
+        const string testStr = "Card 1: 41 48 83 45 4  | 41 48 83 45 4";
+
+        var card = Card.Parse(testStr);
+        var result = card.Points();
+
+        Assert.That(result, Is.EqualTo(16));
+    }
     
 }
--- AdventOfCode.Tests/Day3Tests.cs
+++ AdventOfCode.Tests/Day3Tests.cs
@@ -1,127 +1,12 @@
 namespace AdventOfCode.Tests;
 using AdventOfCode;
 
-public class Day02Tests
+public class Day03Tests
 {
     [SetUp]
     public void Setup()
     {
     }
 
-    [Test]
-    public void GameParser_Parses_Game()
-    {
-        const string testGameStr = "Game 16: 3 red, 3 green; 6 green, 4 red, 3 blue; 3 red, 4 blue; 4 blue, 2 red, 4 green";
-
-        var result = GameParser.ParseGame(testGameStr);
-
-        var expected = new Game { Rounds =
-            [
-                new Round { Red = 3, Green = 3 }, new Round { Red = 4, Green = 6, Blue = 3 },
-                new Round { Red = 3, Blue = 4 }, new Round { Red = 2, Green = 4, Blue = 4 }
-            ]
-        };
-        
-        Assert.That(result.Rounds, Has.Count.EqualTo(expected.Rounds.Count));
-    }
-
-    [Test]
-    public void GameParser_Parses_GameId()
-    {
-        const string testStr = "Game 16";
-
-        var result = GameParser.ParseGameId(testStr);
-        
-        Assert.That(result, Is.EqualTo(16));
-    }
     
-    [Test]
-    public void GameParser_Parses_A_Round()
-    {
-        const string testStr = "6 green, 4 red, 3 blue";
-
-        var result = GameParser.ParseRound(testStr);
-        
-        Assert.That(result.Red, Is.EqualTo(4));
-        Assert.That(result.Green, Is.EqualTo(6));
-        Assert.That(result.Blue, Is.EqualTo(3));
-    }
-    
-    [Test]
-    public void GameParser_Parses_Incomplete_Round()
-    {
-        const string testStr = "6 green, 3 blue";
-
-        var result = GameParser.ParseRound(testStr);
-        Assert.Multiple(() =>
-        {
-            Assert.That(result.Green, Is.EqualTo(6));
-            Assert.That(result.Blue, Is.EqualTo(3));
-        });
-    }
-
-    [Test]
-    public void GameValidator_IsValid()
-    {
-        var game = new Game { Id = 1, Rounds = { new Round { Red = 2, Green = 2, Blue = 2 } } };
-        var validValues = new Round { Red = 11, Green = 11, Blue = 11 };
-        
-        var result = GameValidator.IsValid(game, validValues);
-        
-        Assert.That(result, Is.True);
-    }
-    
-    [Test]
-    public void GameValidator_IsValidIncompleteRound()
-    {
-        var game = new Game { Id = 1, Rounds = { new Round { Red = 2, Blue = 2 } } };
-        var validValues = new Round { Red = 11, Green = 11, Blue = 11 };
-        
-        var result = GameValidator.IsValid(game, validValues);
-        
-        Assert.That(result, Is.True);
-    }
-    
-    [Test]
-    public void GameValidator_IsNotValid()
-    {
-        var game = new Game { Id = 1, Rounds = { new Round { Red = 2, Green = 2, Blue = 2 } } };
-        var validValues = new Round { Red = 1, Green = 11, Blue = 11 };
-        
-        var result = GameValidator.IsValid(game, validValues);
-        
-        Assert.That(result, Is.False);
-    }
-    
-    [Test]
-    public void GameValidator_GetValidGames()
-    {
-        var game1 = new Game { Id = 1, Rounds = { new Round { Red = 2, Green = 2, Blue = 2 } } };
-        var game2 = new Game { Id = 2, Rounds = { new Round { Red = 2, Green = 2, Blue = 2 } } };
-        var game3 = new Game { Id = 3, Rounds = { new Round { Red = 22, Green = 2, Blue = 2 } } };
-        var validValues = new Round { Red = 11, Green = 11, Blue = 11 };
-
-        var games = new List<Game> { game1, game2, game3 };
-        
-        var result = GameValidator.GetValidGames(games, validValues);
-        
-        Assert.That(result, Has.Count.EqualTo(2));
-    }
-
-    [Test]
-    public void GamePowerCalculator_CalculatePower()
-    {
-        // Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-        var game2 = new Game { Id = 3, Rounds = { new Round { Red = 20, Green = 8, Blue = 6 }, new Round { Red = 4, Green = 13, Blue = 5 }, new Round { Red = 1, Green = 5 } } };
-        var expectedPower = new Round { Red = 20, Green = 13, Blue = 6 };
-
-        var result = GamePowerCalculator.CalculatePower(game2);
-        Assert.Multiple(() =>
-        {
-            Assert.That(result.Red, Is.EqualTo(expectedPower.Red));
-            Assert.That(result.Blue, Is.EqualTo(expectedPower.Blue));
-            Assert.That(result.Green, Is.EqualTo(expectedPower.Green));
-            Assert.That(result.Power, Is.EqualTo(1560));
-        });
-    }
 }
